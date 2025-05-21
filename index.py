976f2f7d50bef6efa9408bf5ddfce6cf42f1e7f4
vivek birla <birlavivek2004@gmail.com>
2025-05-22 02:34:32 +0530

    ADDED FEW LINES

--- index.py
+++ index.py
@@ -1,3 +1,10 @@
-str = "I am vivek birla"
-print(str.endswith("la"))
-print(str.endswith("re"))
+# str = "I am vivek birla"
+# print(str.endswith("la"))
+# print(str.endswith("re"))
+
+a = float(input("Enter a number"))
+b = float(input("Enter another number"))
+c = a + b 
+d = a ** b
+print("The sum of these two values  is ",c)
+print("the answer to the exponent will be", d)
